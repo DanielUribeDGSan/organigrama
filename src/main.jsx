aec6581c8bc6631e75ec8824f4adb91caee12ec6
DanielUribeDGSan <daniel.uribe.garcia07@gmail.com>
2025-04-30 13:51:52 -0600

    feat: resolve bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import OrganigramaApp from "./OrganigramaApp.jsx";
+import OrganigramaApp from "./OrganigramaApp";
 
 // import './styles/utils.scss';
 import "./styles/index.scss";
